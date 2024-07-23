19657d05cf52a75382ef322f38c06c000398ec8d
Tobani Gonzalez <123999381+TobaniGJ1@users.noreply.github.com>
2024-07-22 21:16:58 -0400

    OCP COMMIT

---
+++ Course.cs
@@ -0,0 +1,65 @@
+using System;
+
+public abstract class Course
+{
+    public int CourseId { get; set; }
+    public string Title { get; set; }
+
+    public abstract void Subscribe(Student std);
+}
+
+public class OnlineCourse : Course
+{
+    public override void Subscribe(Student std)
+    {
+        // Código para suscribirse a un curso en línea
+        Console.WriteLine($"{std.Name} se ha suscrito al curso en línea: {Title}");
+    }
+}
+
+public class OfflineCourse : Course
+{
+    public override void Subscribe(Student std)
+    {
+        // Código para suscribirse a un curso presencial
+        Console.WriteLine($"{std.Name} se ha suscrito al curso presencial: {Title}");
+    }
+}
+
+public class HybridCourse : Course
+{
+    public override void Subscribe(Student std)
+    {
+        // Código para suscribirse a un curso híbrido
+        Console.WriteLine($"{std.Name} se ha suscrito al curso híbrido: {Title}");
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+
+    public Student(string name)
+    {
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear instancias de cursos
+        Course onlineCourse = new OnlineCourse { CourseId = 1, Title = "Curso de Programación en Línea" };
+        Course offlineCourse = new OfflineCourse { CourseId = 2, Title = "Curso de Programación Presencial" };
+        Course hybridCourse = new HybridCourse { CourseId = 3, Title = "Curso de Programación Híbrido" };
+
+        // Crear instancia de estudiante
+        Student student = new Student("Juan Pérez");
+
+        // Suscribir estudiante a los cursos
+        onlineCourse.Subscribe(student);
+        offlineCourse.Subscribe(student);
+        hybridCourse.Subscribe(student);
+    }
+}
